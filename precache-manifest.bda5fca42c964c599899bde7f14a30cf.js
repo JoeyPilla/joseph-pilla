7be971d7f4f92ddbdea85e6bd02074dbb06d5e36
JoeyPilla <Pilla.4@osu.edu>
2019-03-01 20:06:43 -0600

    Updates

---
+++ precache-manifest.bda5fca42c964c599899bde7f14a30cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "www.pilla.dev/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0d70d31deba0c9765acb",
+    "url": "www.pilla.dev/static/js/runtime~main.0d70d31d.js"
+  },
+  {
+    "revision": "ac88d5baf80a754b46c1",
+    "url": "www.pilla.dev/static/js/main.ac88d5ba.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "www.pilla.dev/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "ac88d5baf80a754b46c1",
+    "url": "www.pilla.dev/static/css/main.1d0b8b82.chunk.css"
+  },
+  {
+    "revision": "ba70f668e6e3a8dfc064ec8657fc7925",
+    "url": "www.pilla.dev/index.html"
+  }
+];
