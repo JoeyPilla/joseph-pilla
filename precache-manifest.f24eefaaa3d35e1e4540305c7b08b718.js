ca7b0774ce3ee4a0184b05430d992664ec5ed4b5
JoeyPilla <Pilla.4@osu.edu>
2019-03-31 20:54:38 -0500

    Updates

---
+++ precache-manifest.f24eefaaa3d35e1e4540305c7b08b718.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "307e25de7b4ca027dd85ab5ef1f0be2d",
+    "url": "/static/media/background.307e25de.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5ca0d52d8d28f8058221",
+    "url": "/static/js/main.e56cd2c9.chunk.js"
+  },
+  {
+    "revision": "118b63faf21fa9bfdbd7",
+    "url": "/static/js/2.dd475be1.chunk.js"
+  },
+  {
+    "revision": "5ca0d52d8d28f8058221",
+    "url": "/static/css/main.d7b952d7.chunk.css"
+  },
+  {
+    "revision": "fd7df27e862293ef1e109cd146406ce0",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.baabebbd764005f91ff61b75114ec464.js"
+  "/precache-manifest.f24eefaaa3d35e1e4540305c7b08b718.js"
 );
 
 workbox.clientsClaim();
