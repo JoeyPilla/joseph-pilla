776201c0ea4ce6e0646ab063e34c5754a6e0b4fb
joey <pilla.4@osu.edu>
2019-04-08 10:28:19 -0500

    adding a tech list

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,101 +1,53 @@
-import React from 'react'
+import React, {useState} from 'react'
 import PageContainer from './PageContainer';
 import {
   LinkA,
   Text,
   Title,
 } from '../Elements/textStyles';
+import { PROJECTS, TECH } from './data';
 
 export default function Projects() {
+  const [tech, setTech] = useState('')
+  const techList = TECH.map(item => {
+    return <button onClick={() => setTech(item)}>{item}</button>
+  })
+  const projects = PROJECTS.map((project) => {
+    if (project.tech.includes(tech) || tech === '') {
+      return (
+        <>
+          <LinkA
+            href={project.link}
+            page={"projects"}
+            size={"2.25rem"}>
+            {project.title}
+          </LinkA>
+          <Text>{project.description}</Text>
+          <Text>
+            Technologies Utilized: {
+              project.tech.map((tech, i) => {
+                if (i !== project.tech.length - 1) {
+                  return (tech + ', ')
+                } else {
+                  return (tech)
+                }
+              })
+            }
+          </Text>
+        </>
+      )
+    } else {return ''}
+
+  })
+
   return (
     <PageContainer>
       <Title page={"projects"}>Projects</Title>
       <div>
-        <LinkA
-          href='https://github.com/JoeyPilla/devices'
-          page={"projects"}
-          size={"2.25rem"}>
-            Devices Database
-        </LinkA>
-        <Text>
-          Created a full stack website with React and Node to solve an
-          organizational problem within the department. The website
-          enables users to create, read, update, and delete device
-          pages. The website incorporated persistent data storage to
-          archive the uploaded files related to each device. I also
-          designed a graphQL server with an express.js backend to
-          interface with a persistent online mongo database. I created
-          a scraping script to import the already existing files from
-          the old file system int.
-        </Text>
-        <Text>
-          Technologies Utilized: Apollo, CSS, Express, Git, GraphQL,
-          HTML, Javascript, MongoDB, Mongoose, React, React Router,
-          and Styled Components.
-        </Text>
+      {techList}
       </div>
       <div>
-          <LinkA
-            page={"projects"}
-            size={"2.25rem"}>
-            Meeting Scheduler
-          </LinkA>
-        <Text>
-          The department needed a website to give the client the
-          ability to schedule tours of the lab. I created a single
-          page application to email the head of the department and add
-          the tour to a temporary calendar for approval.
-        </Text>
-        <Text>
-          Technologies used: CSS, Git, Google Calendar, HTML,
-          Javascript, React, and Styled Components.
-        </Text>
-      </div>
-      <div>
-        <LinkA
-          href='https://github.com/JoeyPilla/joseph-pilla'
-          page={"projects"}
-          size={"2.25rem"}>
-          Pilla.dev
-        </LinkA>
-        <Text>
-          Developed my personal website.
-        </Text>
-        <Text>
-          Technologies used: CSS, Git, Github Pages, HTML, Javascript,
-          React, React Router, React Spring, and Styled Components.
-        </Text>
-      </div>
-      <div>
-        <LinkA
-          page={"projects"}
-          size={"2.25rem"}>
-          To Do App
-        </LinkA>
-        <Text>
-            Created a website to maintain my To Do list.
-          </Text>
-        <Text>
-          Technologies used: Expo, Moment, Native-base, React, and
-          React Native.
-        </Text>
-      </div>
-      <div>
-        <LinkA
-          href='https://github.com/JoeyPilla/task-manager'
-          page={"projects"}
-          size={"2.25rem"}>
-          Task Manager
-        </LinkA>
-        <Text>
-            Developed an app using React Native to optimize my
-            workflow.
-          </Text>
-        <Text>
-          Technologies used: CSS, Git, Google calendar, HTML,
-          Javascript, React-Native, React Router, React Spring, and
-          Styled Components.
-        </Text>
+        {projects}
       </div>
     </PageContainer>
   )
---
+++ src/Components/data.js
@@ -0,0 +1,101 @@
+export const PROJECTS =[
+  {
+    title: 'Devices Database',
+    link: 'https://github.com/JoeyPilla/devices',
+    description: 'Created a full stack website with React and Node to solve an organizational problem within the department.The website enables users to create, read, update, and delete device pages.The website incorporated persistent data storage to archive the uploaded files related to each device.I also designed a graphQL server with an express.js backend to interface with a persistent online mongo database.I created a scraping script to import the already existing files from the old file system int.',
+    tech: [
+      'Apollo',
+      'CSS',
+      'Express',
+      'Git',
+      'GraphQL',
+      'HTML',
+      'Javascript',
+      'MongoDB',
+      'Mongoose',
+      'React',
+      'React Router',
+      'Styled Components',
+    ]
+  },
+  {
+    title: 'Meeting Scheduler',
+    link: '',
+    description: 'The department needed a website to give the client the ability to schedule tours of the lab. I created a single page application to email the head of the department and add the tour to a temporary calendar for approval.',
+    tech: [
+      'CSS',
+      'Git',
+      'Google Calender',
+      'HTML',
+      'Javascript',
+      'React',
+      'Styled Components',
+    ]
+  },
+  {
+    title: 'Pilla.dev',
+    link: 'https://github.com/JoeyPilla/joseph-pilla',
+    description: 'Developed my personal website.',
+    tech: [
+      'CSS',
+      'Git',
+      'Github Pages',
+      'HTML',
+      'Javascript',
+      'React',
+      'React Router',
+      'React Spring',
+      'Styled Components',
+    ]
+  },
+  {
+    title: 'To Do App',
+    link: '',
+    description: 'Created a website to maintain my To Do list.',
+    tech: [
+      'Expo',
+      'Moment',
+      'Native-base',
+      'Javascript',
+      'React',
+      'React Native',
+    ]
+  },
+  {
+    title: 'Task Manager',
+    link: 'https://github.com/JoeyPilla/task-manager',
+    description: 'Developed an app using React Native to optimize my workflow.',
+    tech: [
+      'CSS',
+      'Git',
+      'Google Calendar',
+      'HTML',
+      'Javascript',
+      'React',
+      'React Native',
+      'React Router',
+      'React Spring',
+      'Styled Components',
+    ]
+  },
+]
+export const TECH = [
+  'Apollo',
+  'CSS',
+  'Expo',
+  'Express',
+  'Git',
+  'Google Calendar',
+  'GraphQL',
+  'HTML',
+  'Javascript',
+  'Moment',
+  'MongoDB',
+  'Mongoose',
+  'Native-base',
+  'React Native',
+  'React Router',
+  'React Spring',
+  'React',
+  'Styled Components',
+]
