187d6d3e6d2e66367bf1604419f6a1fa6e40007b
JoeyPilla <Pilla.4@osu.edu>
2019-08-07 21:25:45 -0500

    Updates

---
+++ precache-manifest.8479aa348998df4258785ce08acb5e85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "515c7a1255ff195760b25e2f435b0570",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6bda3c36b30db94e59ff",
+    "url": "/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "9fb9b87d727294b0c826",
+    "url": "/static/js/2.7c257254.chunk.js"
+  },
+  {
+    "revision": "6bda3c36b30db94e59ff",
+    "url": "/static/js/main.67c5428a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "307e25de7b4ca027dd85ab5ef1f0be2d",
+    "url": "/static/media/background.307e25de.jpg"
+  }
+]);
